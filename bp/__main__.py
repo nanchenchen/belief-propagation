dd206a21dd5c2b36a9db233c2c85d2cd0ec60b1b
Nan-Chen Chen <nanchen.chen@gmail.com>
2015-02-24 16:09:56 -0800

    adding exec options

--- bp/__main__.py
+++ bp/__main__.py
@@ -2,14 +2,41 @@ import util.BIFParser as parser
 import util.bcgraph as bcgraph
 import sys
 
-if len(sys.argv) < 2:
+
+def usage():
     print >>sys.stderr, "Usage: python %s <.bif file path> [-o output file] [-t threshold]\n" % sys.argv[0]
     print >>sys.stderr, "Options:"
     print >>sys.stderr, "   -o, --output \t output file name, default to 'result.txt'"
     print >>sys.stderr, "   -t, --threshold \t threshold for convergence default to 1e-10"
+
+if len(sys.argv) < 2:
+    usage()
     exit()
 
-f = open("%s" %sys.argv[1],"r")
+threshold = 1e-10
+result_file = "result.txt"
+
+for i in range(1, len(sys.argv)):
+    if sys.argv[i] == "-o" or sys.argv[i] == "--output":
+        try:
+            result_file = sys.argv[i + 1]
+        except IndexError:
+            usage()
+            exit()
+    elif sys.argv[i] == "-t" or sys.argv[i] == "--threshold":
+        try:
+            threshold = float(sys.argv[i + 1])
+        except IndexError:
+            usage()
+            exit()
+
+try:
+    f = open("%s" %sys.argv[1],"r")
+except IOError:
+    print >>sys.stderr, "Cannot find the .BIF file!\n"
+    usage()
+    exit()
+
 BIF = f.readlines()
 
 BIF = parser.fixWhiteSpace(BIF)
@@ -20,7 +47,7 @@ f.close()
 graph = bcgraph.BCGraph()
 graph.build_graph_from_BN(nodes)
 
-threshold = 1e-10
+
 
 while True:
     diff_value = graph.var_to_factor_propagate()
